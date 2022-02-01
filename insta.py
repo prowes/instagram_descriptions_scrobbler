66c0cbe0c51cb06b5d16a467610dac7b7814fecc
ivan prowes <locust94@gmail.com>
2022-02-01 21:47:35 +0200

    Fix fail if description is empty & use csv instead of txt

--- insta.py
+++ insta.py
@@ -32,35 +32,46 @@ class instag():
             pass
 
     def get_text(self):
-        self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'C4VMK')))
-        description = self.browser.find_element_by_class_name('C4VMK')
-        ret = (f'link: {self.browser.current_url}, text:{description.text}\n\n')
+        try:
+            self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'C4VMK')))
+            description = self.browser.find_element_by_class_name('C4VMK')
+            print(type(description.text))
+            description_text = description.text.strip('\n')
+            print(description_text)
+        except:
+            description_text = ''
+            print('the description is empty')
+        ret = (f'{self.browser.current_url},{description_text}\n')
         webdriver.ActionChains(self.browser).send_keys(Keys.RIGHT).perform()
         return ret
 
-    def create_folder_and_file(self, file_location, amount_of_posts):
-        if not os.path.exists(file_location):
-            os.mkdir(file_location)
-        f = open(rf'{file_location}\\content.txt', 'w+', encoding='utf-8')
+    def create_folder_and_file(self, amount_of_posts):
+        if not os.path.exists(self.file_location):
+            os.mkdir(self.file_location)
+        f = open(rf'{self.file_location}\\content.csv', 'w+', encoding='utf-8')
+        f.write('URL,post description')
         for i in range (0, amount_of_posts):
             f.write(self.get_text())
             print (f'{i} out of {amount_of_posts}')
         f.close()
 
-    def main(self):
+    def ask_arguments(self):
         self.username = input('Please enter username:\n')
         self.password = getpass()
         if len(self.password) < 6:
             print('Password seems to be incorrect, it should have at least 6 symbols, try again')
             exit()
-        account_name = input('Please enter the account name:\n')
-        file_location = input('Please enter the desired location for the file name (ie C:\\folder) \n')
-        if self.username == '' or self.password == '' or account_name == '' or file_location == '':
+        self.account_name = input('Please enter the account name:\n')
+        self.file_location = input('Please enter the desired location for the file name (ie C:\\folder) \n')
+        if self.username == '' or self.password == '' or self.account_name == '' or self.file_location == '':
             print('Please check values and try again, something is empty')
             exit()
-        if ':\\' not in file_location:
+        if ':\\' not in self.file_location:
             print('Incorrect file location entered')
             exit()
+
+    def main(self):
+        self.ask_arguments()
 
         self.browser = webdriver.Chrome()
         self.wait = WebDriverWait(self.browser, 10)
@@ -70,14 +81,14 @@ class instag():
         self.browser.get(link)
         self.accept_cookies()
         self.login()
-        self.browser.get(f'https://www.instagram.com/{account_name}/')
+        self.browser.get(f'https://www.instagram.com/{self.account_name}/')
 
         self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'g47SY')))  # total posts counter
         amount_of_posts = int(self.browser.find_element_by_class_name('g47SY').text)
         all_posts = self.browser.find_elements_by_class_name('v1Nh3')
         all_posts[0].click()  # click the first photo
 
-        self.create_folder_and_file(file_location, amount_of_posts)
+        self.create_folder_and_file(amount_of_posts)
         self.browser.quit()
 
 if __name__ == '__main__':
